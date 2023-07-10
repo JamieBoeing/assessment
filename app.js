5d3e768e679d664ad7c259adb003d8305b2f67ac
jamie boeing <bleupaon@outlook.com>
2023-07-10 18:33:20 -0500

    assess

---
+++ app.js
@@ -0,0 +1,11 @@
+let value = 0
+
+document.getElementById('#down-click').addEventListener('click', down-click)
+document.getElementById('#up-click')addEventListener('click', up-click)
+       
+function upClick (event) {
+  
+}
+function downClick (event) {
+  
+}
